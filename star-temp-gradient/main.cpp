601077bc1344fdcfc129108805b6326d224d8e73
Harrison Chiu <61607959+harrchiu@users.noreply.github.com>
2021-02-21 13:57:00 -0500

    rename + clean

--- star-temp-gradient/main.cpp
+++ star-temp-gradient/main.cpp
@@ -1,48 +1,49 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
+typedef long double ld;
 
 // constants in standard mks
-long double pi = 3.141592654;
-long double grav_const = 6.67408*pow(10,-11);
+ld pi = 3.141592654;
+ld grav_const = 6.67408*pow(10,-11);
 
-long double k = 1.38*pow(10,-23);
-long double u = 0.61;
-long double m_h = 1.67*pow(10,-27);
-long double T_eighty_r = 1*pow(10,6);
+ld k = 1.38*pow(10,-23);
+ld u = 0.61;
+ld m_h = 1.67*pow(10,-27);
+ld T_eighty_r = 1*pow(10,6);
 
-long double a = 7.56*pow(10,-16);
-long double r_sun = 638000000;
-long double kappa_star = 0.001;
+ld a = 7.56*pow(10,-16);
+ld r_sun = 638000000;
+ld kappa_star = 0.001;
 
-long double c_p = 2.5;
-long double c_v = 1.5;
-long double gamma_star = c_p/c_v;
+ld c_p = 2.5;
+ld c_v = 1.5;
+ld gamma_star = c_p/c_v;
 
-long double initialDensity = 150000; // 150 kg/m^3
+ld initialDensity = 150000; // 150 kg/m^3
 
-long double c_light = 3*pow(10,8);
+ld c_light = 3*pow(10,8);
 
-long double L = 3.86*pow(10,26);
+ld L = 3.86*pow(10,26);
 
 // calculate density at given radius
-long double density(long double radius){
+ld density(long double radius){
     return initialDensity * (1-radius/r_sun);
 }
 
 // calculate mass of sphere at given radius
-long double mass(long double radius){
+ld mass(long double radius){
     return 4*pi*initialDensity*( pow(radius,3)/3 - pow(radius,4)/(4*r_sun) );
 }
 
 // calculate pressure of sphere at given radius
-long double pressure(long double r){
+ld pressure(long double r){
     return -4*pi*grav_const*initialDensity*initialDensity*((r*r*r*r)/(16*r_sun*r_sun)-(7*r*r*r)/(36*r_sun)+(r*r)/6)+(5/36*pi*grav_const*initialDensity*initialDensity*r_sun*r_sun);
     //return pi*grav_const*pow(initialDensity,2) * ( -4*( pow(r,4)/(16*pow(r_sun,2)) - 7*pow(r,3)/(36*r_sun) + pow(r,2)/6 ) + (5/36)*pow(r_sun,2) );
 }
 
 // calculate final temperature at given radius
-long double temperature(long double r){
+ld temperature(long double r){
     //return u*m_h*pressure(radius)/k/density(radius);
     return 0.1*(5*pi*grav_const*u*m_h)/(36*k)*initialDensity*r_sun*r_sun*(1+r/(r_sun*r_sun)-(19*r*r)/(5*r_sun*r_sun)+(9*r*r*r)/(5*r_sun*r_sun*r_sun))+1.3125*pow(10,7);
 
